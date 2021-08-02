5ca0a851f16d07f44902f08aea96f6344c7cc764
unknown <darlenegarciaarias@icloud.com>
2021-08-01 21:15:05 -0400

    y-app

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,6 +21,21 @@ export const fetchSmurfs = () => {
 
     }
 }
+export const fetchStart = () => {
+    return ({type:FETCH_START})
+}
+export const fetchSuccess = (smurf) => {
+    return ({type:FETCH_SUCCESS, payload: error})
+}
+export const fetchFail = (error) => {
+    return ({type:FETCH_FAIL, payload: error})
+}
+export const addSmurf = (smurf) => {
+    return({ADD_SMURF, payload: smurf})
+}
+export const setError = (error) => {
+    return({type:SET_ERROR, payload: error})
+}
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
